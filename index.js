58fa9013e29853873c5e9fdf464e3bcb24b02296
JaeBEE <johnnae908@yahoo.com>
2023-11-17 10:07:28 -0800

    added view employee function and add department, both working

--- index.js
+++ index.js
@@ -58,6 +58,32 @@ function viewRoles(){
   })
 }
 
+function viewEmployees(){
+  const query= "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS Department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id=department.id LEFT JOIN employee manager ON manager.id=employee.manager_id"
+ 
+  db.query(query, function(err, results){
+    if (err) throw err;
+    console.table(results);
+    init();
+  })
+ }
+
+ function addDepartment(){
+   inquirer.prompt([
+     {
+       type: 'input',
+       name: 'name',
+       message: 'What is the name of the department?'
+     }
+   ]).then((department) => {
+     db.query("INSERT INTO department(name) VALUES (?)", department.name, function(err, results){
+       if (err) throw err;
+       console.log(`The ${department.name} Department was added`);
+       init();
+     })
+   })
+ }
+
 
 
 init()
