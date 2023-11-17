fb0eb94919d4e2b42c54f3f65690b36846d3aada
JaeBEE <johnnae908@yahoo.com>
2023-11-17 10:01:09 -0800

    created main function with switch case and functions to view departments and roles. tested and it is working

--- index.js
+++ index.js
@@ -1,56 +1,63 @@
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+const { add } = require('lodash');
 
 
-
-// Create a employee
-
-  const sql = `INSERT INTO movies (first_name)
-    VALUES (?)`;
-  const params = [body.first_name];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
+function init(){
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices:["View Departments", "View Roles", "View Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role", "Exit"]
     }
-    res.json({
-      message: 'success',
-      data: body
-    });
-  });
+  ]).then((answers) => {
+    switch (answers.choice) {
+      case "View Departments":
+        viewDepartments();
+        break;
+      case "View Roles": 
+        viewRoles();
+        break;
+      case "View Employees":
+        viewEmployees();
+        break;
+      case "Add Department":
+        addDepartment();
+        break;
+      case "Add Role": 
+        addRole();
+        break;
+      case "Add Employee":
+        addEmployee()
+        break;
+      case "Update Employee Role":
+        updateEmployeeRole();
+        break;
 
-
-
-// Delete a employee
-
-  const sql = `DELETE FROM employee WHERE id = ?`;
-  const params = [req.params.id];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.statusMessage(400).json({ error: res.message });
-    } else if (!result.affectedRows) {
-      res.json({
-      message: 'Employee not found'
-      });
-    } else {
-      res.json({
-        message: 'deleted',
-        changes: result.affectedRows,
-        id: req.params.id
-      });
+      default:
+        db.end();
+        break;
     }
-  });
+  })
+}
+
+function viewDepartments(){
+  db.query("SELECT * FROM department", function(err, results){
+    if (err) throw err;
+    console.table(results);
+    init();
+  })
+}
+
+function viewRoles(){
+  db.query("SELECT role.title, department.name AS Department, role.salary FROM role LEFT JOIN department ON role.department_id=department.id", function(err, results){
+    if (err) throw err;
+    console.table(results);
+    init();
+  })
+}
 
 
 
-  const sql = `SELECT employee.first_name AS employee, department.department FROM department LEFT JOIN employee ON department.employee_id = employee.id ORDER BY employee.first_name;`;
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
+init()
