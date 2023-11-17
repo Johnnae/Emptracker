53a9066d849c814334c809b3852a837d78ac1163
JaeBEE <johnnae908@yahoo.com>
2023-11-17 10:23:10 -0800

    updated index.js

--- index.js
+++ index.js
@@ -84,6 +84,118 @@ function viewEmployees(){
    })
  }
 
+ function addRole(){ 
+    db.query("SELECT * FROM department", function(err, results){
+      if (err) throw err;
+      const departmentChoices = results.map((department) =>({
+          name: department.name,
+          value: department.id
+      }))
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'What is the name of the role?'
+        },
+        {
+          type: 'input',
+          name: 'salary',
+          message:"What is the role's salary?"
+        },
+        {
+          type: 'list',
+          name: 'department_id',
+          message: 'What department does the role belong to?',
+          choices: departmentChoices
+        }
+        
+       ]).then((role) => {
+         db.query("INSERT INTO role SET ?", role, function(err, results){
+           if (err) throw err;
+           console.log(`The ${role.title} role was added`);
+           init();
+         })
+       })
+
+    })
+  }
+
+  function addEmployee(){
+    db.query("SELECT * FROM role", function(err, results){
+      if (err) throw err;
+      const roleChoices = results.map((role) =>({
+          name: role.title,
+          value: role.id
+      }))
+
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'first_name',
+          message: "What is the employee's first name?"
+        },
+        {
+          type: 'input',
+          name: 'last_name',
+          message:'What is the employee last name?'
+        },
+        {
+          type: 'list',
+          name: 'role_id',
+          message: "What is the employee's role?",
+          choices: roleChoices
+        }
+      ]).then((employee) => {
+        db.query("INSERT INTO employee SET ?", employee, function(err, results){
+          if (err) throw err;
+          console.log(`The ${employee.first_name} ${employee.last_name} employee was added`);
+          init();
+        })
+      })
+    })
+  }
+
+  function updateEmployeeRole(){
+
+    db.query("SELECT * FROM employee", function(err, results){
+      if (err) throw err;
+      const employeeChoices = results.map((employee) =>({
+          name: employee.first_name + " " + employee.last_name,
+          value: employee.id
+      }))
+
+      db.query("SELECT * FROM role", function(err, results){
+        if (err) throw err;
+        const roleChoices = results.map((role) =>({
+            name: role.title,
+            value: role.id
+        }))
+
+        inquirer.prompt([
+          {
+            type: 'list',
+            name: 'employee_id',
+            message: "Which employee's role do you want to update?",
+            choices: employeeChoices
+          },
+          {
+            type: 'list',
+            name: 'role_id',
+            message: "Which role do you want to assign the selected employee?",
+            choices: roleChoices
+          }
+        ]).then((employee) => {
+          db.query("UPDATE employee SET role_id = ? WHERE id = ?", [employee.role_id, employee.employee_id], function(err, results){
+            if (err) throw err;
+            console.log(`The employee's role was updated`);
+            init();
+          })
+        })
+      })
+    })
+  }
+ 
+
 
 
 init()
